2529769296117f33454452525b67362d93322aa2
Demur Rumed <gunkmute@gmail.com>
2015-06-08 04:18:16 +0000

    Uglify, update package.json

--- src/mkpixi.js
+++ src/mkpixi.js
@@ -10,7 +10,10 @@ child_process.execFile("find", [".", "-type", "f"], function(err, stdout, stderr
 	});
 	files.splice(0, 0, "../pixi.js");
 	child_process.execFile("mkcjs", files, function(err, stdout, stderr){
-		console.log(stdout, stderr);
-		process.exit();
+		if (stdout || stderr) console.log(stdout, stderr);
+		child_process.execFile("uglifyjs", ["../pixi.js", "--screw-ie8", "-mco", "../pixi.min.js"], function(err, stdout, stderr){
+			if (stdout || stderr)  console.log(stdout, stderr);
+			process.exit();
+		});
 	});
 });
